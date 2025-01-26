128cd358864ec1f522eed78f423cdb1aefa3c7c3
Maciejj99 <maciek1999s63@gmail.com>
2025-01-26 13:50:07 +0100

    Update data1.sql

---
+++ data1.sql
@@ -0,0 +1,195 @@
+INSERT INTO Pilot (imie, nazwisko, adres, telefon, email)
+VALUES 
+    ('Jan', 'Kowalski', 'ul. Szczęśliwa 12, Warszawa', '555123456', 'jan.kowalski@example.com'),
+    ('Anna', 'Nowak', 'ul. Kwiatowa 4, Kraków', '555234567', 'anna.nowak@example.com'),
+    ('Piotr', 'Wiśniewski', 'ul. Leśna 7, Gdańsk', '555345678', 'piotr.wisniewski@example.com'),
+    ('Magdalena', 'Zielińska', 'ul. Słoneczna 15, Wrocław', '555456789', 'magdalena.zielinska@example.com'),
+    ('Marek', 'Kamiński', 'ul. Różana 10, Poznań', '555567890', 'marek.kaminski@example.com'),
+    ('Ewa', 'Lewandowska', 'ul. Ogrodowa 3, Lublin', '555678901', 'ewa.lewandowska@example.com'),
+    ('Tomasz', 'Nowicki', 'ul. Polna 21, Szczecin', '555789012', 'tomasz.nowicki@example.com'),
+    ('Katarzyna', 'Mazur', 'ul. Krótka 8, Katowice', '555890123', 'katarzyna.mazur@example.com'),
+    ('Łukasz', 'Zawadzki', 'ul. Jagiellońska 6, Toruń', '555901234', 'lukasz.zawadzki@example.com'),
+    ('Dominika', 'Sikorska', 'ul. Prusa 22, Bydgoszcz', '555012345', 'dominika.sikorska@example.com');
+
+
+
+INSERT INTO Klient (imie, nazwisko, adres, telefon, email, data_rejestracji)
+VALUES
+	('Andrzej', 'Kaczmarek', 'ul. Jagodowa 45, Kraków', '555132645', 'andrzej.kaczmarek@example.com', '2023-01-22'),
+    ('Martyna', 'Wesołowska', 'ul. Wesoła 18, Warszawa', '555243756', 'martyna.wesolowska@example.com', '2023-02-10'),
+    ('Michał', 'Chmielewski', 'ul. Jesionowa 30, Gdańsk', '555354867', 'michal.chmielewski@example.com', '2023-03-07'),
+    ('Julia', 'Grabowska', 'ul. Spokojna 4, Wrocław', '555465978', 'julia.grabowska@example.com', '2023-04-11'),
+    ('Krzysztof', 'Duda', 'ul. Olsztyńska 12, Poznań', '555576089', 'krzysztof.duda@example.com', '2023-05-16'),
+    ('Natalia', 'Szczepańska', 'ul. Zielona 10, Lublin', '555687190', 'natalia.szczepanska@example.com', '2023-06-01'),
+    ('Sebastian', 'Pawłowski', 'ul. Szkolna 9, Szczecin', '555798201', 'sebastian.pawlowski@example.com', '2023-07-09'),
+    ('Monika', 'Stasiak', 'ul. Wiejska 22, Katowice', '555809312', 'monika.stasiak@example.com', '2023-08-18'),
+    ('Adam', 'Rutkowski', 'ul. Lawendowa 19, Toruń', '555910423', 'adam.rutkowski@example.com', '2023-09-14'),
+    ('Paulina', 'Jankowska', 'ul. Szeroka 7, Bydgoszcz', '555021534', 'paulina.jankowska@example.com', '2023-10-07'),
+    ('Mateusz', 'Sobczak', 'ul. Kwitnąca 8, Warszawa', '555132645', 'mateusz.sobczak@example.com', '2023-01-29'),
+    ('Zuzanna', 'Błaszczyk', 'ul. Słomiana 15, Kraków', '555243756', 'zuzanna.blaszczyk@example.com', '2023-02-05'),
+    ('Rafał', 'Adamski', 'ul. Brzozowa 21, Gdańsk', '555354867', 'rafal.adamski@example.com', '2023-03-20'),
+    ('Karolina', 'Czarnecka', 'ul. Mokra 8, Wrocław', '555465978', 'karolina.czarnecka@example.com', '2023-04-18'),
+    ('Grzegorz', 'Nowosielski', 'ul. Obrońców 10, Poznań', '555576089', 'grzegorz.nowosielski@example.com', '2023-05-13'),
+    ('Agnieszka', 'Urban', 'ul. Zamkowa 16, Lublin', '555687190', 'agnieszka.urban@example.com', '2023-06-22'),
+    ('Konrad', 'Cieślak', 'ul. Nowa 5, Szczecin', '555798201', 'konrad.cieslak@example.com', '2023-07-02'),
+    ('Maria', 'Kosowska', 'ul. Nadmorska 2, Katowice', '555809312', 'maria.kosowska@example.com', '2023-08-25'),
+    ('Wiktor', 'Tomczak', 'ul. Grunwaldzka 9, Toruń', '555910423', 'wiktor.tomczak@example.com', '2023-09-10'),
+    ('Joanna', 'Drozd', 'ul. Łąkowa 33, Bydgoszcz', '555021534', 'joanna.drozd@example.com', '2023-10-12'),
+    ('Tomasz', 'Ostrowski', 'ul. Warszawska 8, Gdynia', '555132645', 'tomasz.ostrowski@example.com', '2023-01-19'),
+    ('Barbara', 'Białek', 'ul. Starowiejska 12, Wrocław', '555243756', 'barbara.bialek@example.com', '2023-02-25'),
+    ('Kamil', 'Leszczyński', 'ul. Skłodowskiej 7, Lublin', '555354867', 'kamil.leszczynski@example.com', '2023-03-16'),
+    ('Sylwia', 'Kaleta', 'ul. Jaglana 4, Katowice', '555465978', 'sylwia.kaleta@example.com', '2023-04-21'),
+    ('Patryk', 'Chyła', 'ul. Mariacka 8, Toruń', '555576089', 'patryk.chyla@example.com', '2023-05-08'),
+    ('Alicja', 'Piech', 'ul. Orzeszkowej 3, Bydgoszcz', '555687190', 'alicja.piech@example.com', '2023-06-17'),
+    ('Damian', 'Banach', 'ul. Łubinowa 22, Szczecin', '555798201', 'damian.banach@example.com', '2023-07-30'),
+    ('Natalia', 'Kołodziej', 'ul. Bażantowa 5, Lublin', '555809312', 'natalia.kolodziej@example.com', '2023-08-19'),
+    ('Marta', 'Sawicka', 'ul. Sosnowa 10, Katowice', '555910423', 'marta.sawicka@example.com', '2023-09-05'),
+    ('Oskar', 'Sosnowski', 'ul. Lipowa 18, Toruń', '555021534', 'oskar.sosnowski@example.com', '2023-10-22'),
+    ('Klaudia', 'Wróbel', 'ul. Pawia 1, Warszawa', '555132645', 'klaudia.wrobel@example.com', '2023-01-14'),
+    ('Norbert', 'Kasprzak', 'ul. Kościuszki 12, Kraków', '555243756', 'norbert.kasprzak@example.com', '2023-02-28'),
+    ('Olga', 'Lis', 'ul. Wiśniowa 6, Gdańsk', '555354867', 'olga.lis@example.com', '2023-03-03'),
+    ('Emilia', 'Korczak', 'ul. Topolowa 4, Wrocław', '555465978', 'emilia.korczak@example.com', '2023-04-09'),
+    ('Filip', 'Olszewski', 'ul. Parkowa 7, Poznań', '555576089', 'filip.olszewski@example.com', '2023-05-18'),
+    ('Lidia', 'Dobrowolska', 'ul. Jesienna 15, Lublin', '555687190', 'lidia.dobrowolska@example.com', '2023-06-27');
+    
+  
+
+INSERT INTO Trasa (opis_trasy, dlugosc_km, region)
+VALUES
+    ('Objazdowa wycieczka przez Włochy: Rzym, Florencja, Wenecja, Neapol', 1800, 'Włochy'),
+    ('Wycieczka bałkańska: Zagrzeb, Sarajewo, Belgrad, Podgorica', 1500, 'Bałkany'),
+    ('Podróż przez Alpy: Zurych, Innsbruck, Monachium, Genewa', 1200, 'Alpy'),
+    ('Objazdowa wycieczka po Hiszpanii: Madryt, Sewilla, Walencja, Barcelona', 2000, 'Hiszpania'),
+    ('Skandynawska wyprawa: Kopenhaga, Oslo, Sztokholm, Helsinki', 2200, 'Skandynawia'),
+    ('Wycieczka przez Francję: Paryż, Lyon, Marsylia, Nicea', 1300, 'Francja'),
+    ('Wielka objazdówka po Niemczech: Berlin, Hamburg, Frankfurt, Monachium', 1400, 'Niemcy'),
+    ('Podróż przez Europę Środkową: Praga, Wiedeń, Bratysława, Budapeszt', 1100, 'Europa Środkowa'),
+    ('Brytyjska ekspedycja: Londyn, Edynburg, Glasgow, Manchester', 900, 'Wielka Brytania'),
+    ('Bałtycka przygoda: Wilno, Ryga, Tallin, Helsinki', 800, 'Region Bałtycki');
+    
+
+INSERT INTO SrodekTransportu (typ, nazwa, pojemnosc, uwagi)
+VALUES
+    ('Autobus', 'Mercedes-Benz Sprinter', 20, 'Autobus do przewozu mniejszych grup, klimatyzacja, przestronny'),
+    ('Autobus', 'Volvo 9900', 50, 'Luksusowy autokar, przestronne siedzenia, toaleta, Wi-Fi'),
+    ('Autobus', 'Scania Touring', 55, 'Autobus do długich tras, klimatyzacja, wygodne siedzenia'),
+    ('Autobus', 'MAN Lion\'s Coach', 48, 'Nowoczesny autokar, system audio, telewizory na pokładzie, Wi-Fi'),
+    ('Autobus', 'Setra 516 HD', 53, 'Autokar o wysokim standardzie, skórzane siedzenia, duży bagażnik'),
+    ('Autobus', 'Iveco Crossway', 45, 'Autobus przeznaczony do turystyki krajowej, klimatyzacja, przestronny'),
+    ('Autobus', 'Neoplan Cityliner', 56, 'Przestronny i komfortowy autokar, ekran, Wi-Fi, toaleta'),
+    ('Autobus', 'Mercedes-Benz Tourismo', 50, 'Autokar turystyczny z dużym bagażnikiem, klimatyzacja, Wi-Fi'),
+    ('Autobus', 'Solaris Urbino 12', 80, 'Autobus miejski, dostosowany do transportu publicznego, niski wjazd'),
+    ('Autobus', 'MAN Lion\'s City', 100, 'Autobus miejski do transportu publicznego, przestronny, klimatyzacja');
+
+
+
+INSERT INTO Wycieczka (id_trasy, id_pilota, liczba_miejsc, koszt, data_poczatku, data_konca, opis_dodatkowy)
+VALUES
+	(1, 1, 20, 1500.00, '2025-03-01', '2025-03-07', 'Objazdowa wycieczka przez Włochy: Rzym, Florencja, Wenecja, Neapol. Zwiedzanie największych skarbów włoskiej kultury, od Wiecznego Miasta po romantyczną Wenecję. Idealna propozycja dla miłośników sztuki i historii!'),
+	(2, 2, 25, 1300.00, '2025-03-15', '2025-03-22', 'Wycieczka bałkańska: Zagrzeb, Sarajewo, Belgrad, Podgorica. Podróż przez Bałkany – region pełen kontrastów, historii i przyrody. Zwiedzanie stolic i urokliwych miast tego fascynującego regionu.'),
+    (3, 3, 30, 2200.00, '2025-04-01', '2025-04-07', 'Podróż przez Alpy: Zurych, Innsbruck, Monachium, Genewa. Wspaniała podróż przez Alpy z malowniczymi widokami górskimi, historycznymi miastami i relaksem w luksusowych kurortach.'),
+    (4, 4, 40, 2500.00, '2025-04-10', '2025-04-17', 'Objazdowa wycieczka po Hiszpanii: Madryt, Sewilla, Walencja, Barcelona. Ciepła atmosfera, sztuka Gaudiego, flamenco, paella i niesamowite zabytki w stolicy Hiszpanii i najważniejszych miastach.'),
+    (5, 5, 45, 2800.00, '2025-05-01', '2025-05-08', 'Skandynawska wyprawa: Kopenhaga, Oslo, Sztokholm, Helsinki. Spotkanie z nowoczesnością Skandynawii, urokliwymi miastami pełnymi zabytków, zielonymi przestrzeniami i niesamowitą aurą nordyckich krajobrazów.'),
+    (6, 6, 35, 1900.00, '2025-05-15', '2025-05-21', 'Wycieczka przez Francję: Paryż, Lyon, Marsylia, Nicea. Przeżyj cudowne dni w Paryżu, francuską prowincję, odkrywaj nadmorską Marsylię oraz spokojny urok Nicei w sercu Lazurowego Wybrzeża.'),
+    (7, 7, 25, 2100.00, '2025-06-01', '2025-06-08', 'Wielka objazdówka po Niemczech: Berlin, Hamburg, Frankfurt, Monachium. Odkryj niemiecką kulturę, bogatą historię i wyjątkowe miasta pełne zabytków, sztuki oraz nowoczesnej architektury.'),
+    (8, 8, 30, 1800.00, '2025-06-10', '2025-06-17', 'Podróż przez Europę Środkową: Praga, Wiedeń, Bratysława, Budapeszt. Wycieczka do czterech pięknych europejskich stolic: czeskiej, austriackiej, słowackiej i węgierskiej, pełnych historii, zabytków i klimatu.'),
+    (9, 9, 50, 1700.00, '2025-07-01', '2025-07-07', 'Brytyjska ekspedycja: Londyn, Edynburg, Glasgow, Manchester. Kultura, zabytki i historia Wysp Brytyjskich w jednej trasie – podróż przez największe brytyjskie miasta, po drodze odkrywając ich unikalny charakter.'),
+    (10, 10, 60, 1300.00, '2025-07-15', '2025-07-22', 'Bałtycka przygoda: Wilno, Ryga, Tallin, Helsinki. Magiczna podróż po krajach nadbałtyckich, odkrywając ich historię, kulturę i niepowtarzalny klimat. Idealna okazja, by przeżyć wyjątkowe przygody nad Morzem Bałtyckim!');
+
+
+
+INSERT INTO Transport (id_wycieczki, id_srodka, uwagi)
+VALUES
+    (1, 1, 'Brak'),
+    (2, 2, 'Brak'),
+    (3, 3, 'Brak'),
+    (4, 4, 'Brak'),
+    (5, 5, 'Brak'),
+    (6, 6, 'Brak'),
+    (7, 7, 'Brak'),
+    (8, 8, 'Brak'),
+    (9, 9, 'Brak'),
+    (10, 10, 'Brak');
+
+  
+    
+INSERT INTO Rezerwacja (id_klienta, id_wycieczki, liczba_miejsc, status, kwota_do_zaplaty, data_platnosci)
+VALUES
+    (1, 1, 2, 'Potwierdzona', 3000.00, '2025-03-01'),
+    (2, 2, 3, 'Oczekująca', 3900.00, NULL),
+    (3, 3, 4, 'Anulowana', 8800.00, NULL),
+    (4, 4, 5, 'Oczekująca', 12500.00, NULL),
+    (5, 5, 6, 'Potwierdzona', 16800.00, '2025-05-01'),
+    (6, 6, 4, 'Oczekująca', 7600.00, NULL),
+    (7, 7, 3, 'Potwierdzona', 6300.00, '2025-06-01'),
+    (8, 8, 5, 'Oczekująca', 9000.00, NULL),
+    (9, 9, 6, 'Potwierdzona', 10200.00, '2025-07-01'),
+    (10, 10, 8, 'Oczekująca', 10400.00, NULL),
+    (11, 1, 1, 'Oczekująca', 1500.00, NULL),
+    (12, 2, 5, 'Potwierdzona', 6500.00, '2025-03-10'),
+    (13, 3, 3, 'Anulowana', 6600.00, NULL),
+    (14, 4, 4, 'Oczekująca', 10000.00, NULL),
+    (15, 5, 2, 'Potwierdzona', 5600.00, '2025-05-02'),
+    (16, 6, 4, 'Oczekująca', 7600.00, NULL),
+    (17, 7, 6, 'Oczekująca', 12600.00, NULL),
+    (18, 8, 5, 'Anulowana', 9000.00, NULL),
+    (19, 9, 4, 'Potwierdzona', 6800.00, '2025-07-10'),
+    (20, 10, 7, 'Oczekująca', 9100.00, NULL),
+    (21, 1, 4, 'Potwierdzona', 6000.00, '2025-03-01'),
+    (22, 2, 3, 'Oczekująca', 3900.00, NULL),
+    (23, 3, 2, 'Potwierdzona', 4400.00, '2025-04-05'),
+    (24, 4, 6, 'Oczekująca', 15000.00, NULL),
+    (25, 5, 3, 'Potwierdzona', 8400.00, '2025-05-10'),
+    (26, 6, 2, 'Anulowana', 3800.00, NULL),
+    (27, 7, 4, 'Oczekująca', 8400.00, NULL),
+    (28, 8, 3, 'Potwierdzona', 5400.00, '2025-06-12'),
+    (29, 9, 2, 'Oczekująca', 3400.00, NULL),
+    (30, 10, 5, 'Potwierdzona', 6500.00, '2025-07-15'),
+    (31, 1, 3, 'Oczekująca', 4500.00, NULL),
+    (32, 2, 4, 'Anulowana', 5200.00, NULL),
+    (33, 3, 1, 'Oczekująca', 2200.00, NULL),
+    (34, 4, 2, 'Potwierdzona', 5000.00, '2025-04-10'),
+    (35, 5, 4, 'Oczekująca', 11200.00, NULL),
+    (36, 6, 6, 'Potwierdzona', 11400.00, '2025-05-20');
+
+
+INSERT INTO Platnosc (id_rezerwacji, kwota, data_platnosci, metoda)
+VALUES
+    (1, 3000.00, '2025-03-01', 'Przelew'),
+    (2, 3900.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca, brak płatności
+    (3, 8800.00, NULL, 'Gotówka'), -- Anulowana rezerwacja
+    (4, 12500.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (5, 16800.00, '2025-05-01', 'Karta'),
+    (6, 7600.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (7, 6300.00, '2025-06-01', 'Karta'),
+    (8, 9000.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (9, 10200.00, '2025-07-01', 'Przelew'),
+    (10, 10400.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (11, 1500.00, '2025-03-01', 'Karta'),
+    (12, 6500.00, '2025-03-10', 'Przelew'),
+    (13, 6600.00, NULL, 'Gotówka'), -- Rezerwacja anulowana
+    (14, 10000.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (15, 5600.00, '2025-05-02', 'Gotówka'),
+    (16, 7600.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (17, 12600.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (18, 9000.00, NULL, 'Gotówka'), -- Rezerwacja anulowana
+    (19, 6800.00, '2025-07-10', 'Karta'),
+    (20, 9100.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (21, 6000.00, '2025-03-01', 'Gotówka'),
+    (22, 3900.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (23, 4400.00, '2025-04-05', 'Przelew'),
+    (24, 15000.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (25, 8400.00, '2025-05-10', 'Karta'),
+    (26, 3800.00, NULL, 'Gotówka'), -- Rezerwacja anulowana
+    (27, 8400.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (28, 5400.00, '2025-06-12', 'Gotówka'),
+    (29, 3400.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (30, 6500.00, '2025-07-15', 'Przelew'),
+    (31, 4500.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (32, 5200.00, NULL, 'Gotówka'), -- Rezerwacja anulowana
+    (33, 2200.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (34, 5000.00, '2025-04-10', 'Przelew'),
+    (35, 11200.00, NULL, 'Gotówka'), -- Rezerwacja oczekująca
+    (36, 11400.00, '2025-05-20', 'Karta');
+    
+    
